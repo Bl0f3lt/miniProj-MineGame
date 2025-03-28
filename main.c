845d2a34f4927d48ffe584f859bac5652c108ba9
sm <Scott.Miller06@outlook.com>
2025-03-28 18:45:30 +0000

    added generateMaterials function. Initial version with hardcode of two materials rather than file reading.

--- main.c
+++ main.c
@@ -36,6 +36,40 @@ struct character {
     invItem_t inventory[numItems];
 };
 
+//Functions
+
+material_t *generateMaterials() {
+    material_t *materialArr = malloc(numItems*sizeof(material_t));
+    if (!materialArr) {
+        return NULL
+    }
+
+    //Hard coding two materials for initial testing. Needs to be updated
+    materialArr[0].ident= 'c';
+    strcpy(materialArr[0].name,"coal");
+    materialArr[0].weight = 50;
+    materialArr[0].value = 3;
+    materialArr[0].size = 5;
+    materialArr[0].mineable = 1;
+
+    materialArr[1].ident= 'e';
+    strcpy(materialArr[1].name,"emerald");
+    materialArr[1].weight = 10;
+    materialArr[1].value = 25;
+    materialArr[1].size = 10;
+    materialArr[1].mineable = 1;
+
+    return materialArr
+
+}
+
 
 int main() {
+    //Test matterial array generation.
+    material_t *materialArr;
+    materialArr = generateMaterials();
+
+    //Print out some details of mat array to confirm good running
+    printf("%s",materialArr[0].name);
+    printf("%s",materialArr[1].name);
 }
