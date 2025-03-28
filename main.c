784813318c4f6d3e9aeeeaaae22cfb2be31d322f
sm <Scott.Miller06@outlook.com>
2025-03-28 23:25:42 +0000

    began implementing generateWorld function. Error in getTotalWeight or setMaterial function. Needs investigating.

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 //Constant value equal to number of collectable items in the Game.
 #define NUMITEMS 2
 
+
+
 //Defining structures
 struct pos {
     int x;
@@ -48,7 +50,7 @@ void setMaterial(material_t *materialPointer, char ident, char name[10], int wei
 
 
 material_t *generateMaterials() {
-    material_t *materialArr = malloc(numItems*sizeof(material_t));
+    material_t *materialArr = malloc(NUMITEMS*sizeof(material_t));
     if (!materialArr) {
         return NULL;
     }
@@ -60,13 +62,52 @@ material_t *generateMaterials() {
     return materialArr;
 }
 
+char **generate2Darr(int x,int y) {
+    char** array;
+    int z;
+
+    //Dynamic memory allocation means memory not reserved at compilation but instead runtime
+    array = (char**)calloc(y,x*sizeof(char));
+    for (z=0;z<y;z++) {
+        array[y] = (char*)malloc(x*sizeof(char));
+    }
+    return array;
+}
+
+int getTotalWeight(material_t *materialArr) {
+    int totalWeight=0;
+    int i;
+    for (i=0;i<NUMITEMS;i++) {
+        totalWeight += materialArr[i].weight;
+        printf("matWeight:  %d  totalWeight: %d\n",materialArr[i].weight,totalWeight);
+    }
+    return totalWeight;
+}
+
+void setGameArrayValues(char **gameArray,material_t *materialArr) {
+    int totalWeight;
+    totalWeight = getTotalWeight(&materialArr);
+    printf("totalWeight: %d",totalWeight);
+}
+
+char **generateWorld(int gameXspan,int gameYspan,material_t *materialArr) {
+    char **gameArray;
+    gameArray = generate2Darr(gameXspan,gameYspan);
+    setGameArrayValues(&gameArray,&materialArr);
+}
+
 
 int main() {
     //Test matterial array generation.
     material_t *materialArr;
     materialArr = generateMaterials();
 
-    //Print out some details of mat array to confirm good running
-    printf("%s\n",materialArr[0].name);
-    printf("%s",materialArr[1].name);
+    //Manual definition of size for now. May change to add dificulty.
+    int gameXspan,gameYspan;
+    gameXspan = 12;
+    gameYspan = 12;
+
+    char **gameArray;
+    gameArray = generateWorld(gameXspan,gameYspan,&materialArr);
+
 }
