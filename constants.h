e8b33f099d91889c10ca6ff17c5ac2ceca3d6e9b
bl0f3lt <Scott.Miller06@outlook.com>
2025-05-02 10:03:33 +0100

    Created structures.h and constants.h

---
+++ constants.h
@@ -0,0 +1,10 @@
+//
+// Created by bl0f3lt on 02/05/2025.
+//
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define NUM_MATERIALS 5
+#define NUM_SHOP_ITEMS 2
+
+#endif //CONSTANTS_H
--- main.c
+++ main.c
@@ -2,84 +2,11 @@
 #include <stdlib.h>
 #include <windows.system.h>
 #include <time.h>
+
+#include "constants.h"
+#include "structures.h"
 //24066799 MineGame C mini Project
 
-//Constant value equal to number of collectable items in the Game.
-#define NUM_MATERIALS 5
-#define NUM_SHOP_ITEMS 2
-
-//Defining structures
-
-//pos structure
-struct pos {
-    int x;
-    int y;
-};
-typedef struct pos pos_t;
-
-//move structure
-struct move {
-    pos_t newPos;
-    char userEntry;
-};
-typedef struct move move_t;
-
-//material strucutre
-struct material {
-    char ident;
-    char name[10];
-    int weight;
-    int value;
-    int size;
-    int mineable;
-};
-typedef struct material material_t;
-
-//materialInvItem structure
-struct materialInvItem {
-    material_t* invMaterial;
-    int quant;
-};
-typedef struct materialInvItem materialInvItem_t;
-
-//shopItem structure
-struct shopItem {
-    int itemIdent;
-    char itemName[10];
-    int cost;
-    int foodValue;
-};
-typedef struct shopItem shopItem_t;
-
-//itemInvItem structure
-struct itemInvItem {
-    shopItem_t* invItem;
-    int quant;
-};
-typedef struct itemInvItem itemInvItem_t;
-
-//buyValid Structure
-struct buyValid {
-    int valid;
-    char reason[20];
-};
-typedef struct buyValid buyValid_t;
-
-//Character Structure
-struct character {
-    pos_t playerPos;
-    int food;
-    int health;
-    int playerMove;
-    int lastCollectionMove;
-    int money;
-    int totalStartMaterials;
-    int materialRem;
-    float playerScore;
-    materialInvItem_t materialInventory[NUM_MATERIALS];
-    itemInvItem_t itemInventory[NUM_SHOP_ITEMS];
-};
-typedef struct character character_t;
 //Functions
 
 void setMaterial(material_t *materialPointer, char ident, char name[10], int weight, int value, int size, int mineable) {
